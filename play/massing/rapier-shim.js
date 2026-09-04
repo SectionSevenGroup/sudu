27489bbbc21aaa189c7090f726c91c87557adcf3
SectionSevenGroup <joe@sectionsevengroup.com>
2026-09-04 16:00:53 -0700

    Remove the previous MASSING Rapier path

--- play/massing/rapier-shim.js
+++
@@ -1,8 +0,0 @@
-import { RAPIER } from '/stack/vendor/stack-deps.js';
-
-export const init = RAPIER.init;
-export const World = RAPIER.World;
-export const RigidBodyDesc = RAPIER.RigidBodyDesc;
-export const ColliderDesc = RAPIER.ColliderDesc;
-export const RigidBodyType = RAPIER.RigidBodyType;
-export const CoefficientCombineRule = RAPIER.CoefficientCombineRule;
